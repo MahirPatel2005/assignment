60db559b200f1189ed096d37aea0b863d59f7186
Mahir <mahir.patel.cg@gmail.com>
2025-11-10 10:52:02 +0530

    Add implementations for BinaryTree and Stack data structures
    Assignment Set B

---
+++ Q2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#define MAX 5
+ 
+int stack[MAX];
+int top = -1;
+ 
+void push(int val) {
+    if(top == MAX - 1)
+        cout << "Stack Overflow!\n";
+    else
+        stack[++top] = val;
+}
+ 
+void pop() {
+    if(top == -1)
+        cout << "Stack Underflow!\n";
+    else
+        cout << "Popped element: " << stack[top--] << "\n";
+}
+ 
+void display() {
+    if(top == -1)
+        cout << "Stack is empty!\n";
+    else {
+        cout << "Stack elements: ";
+        for(int i = top; i >= 0; i--)
+            cout << stack[i] << " ";
+        cout << "\n";
+    }
+}
+ 
+int main() {
+    push(10);
+    push(20);
+    push(30);
+    display();
+    pop();
+    display();
+    return 0;
+}
